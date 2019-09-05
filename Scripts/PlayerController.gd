ad51fcd5d567bb60047a64b92be35ed39d35fc32
Eric da Silva Medeiros <ericdsm@ufrn.edu.br>
2019-09-05 03:59:44 +0000

    Score

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -10,26 +10,28 @@ func _ready():
 
 var move = Vector2()
 
-var score = 0
+onready var GameData = get_node("/root/GameData")
+onready var LivesLabel = get_node("/root/Root/CanvasLayer/LivesLabel")
+onready var ScoreLabel = get_node("/root/Root/CanvasLayer/ScoreLabel")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
-		move.x = 100
+		move.x = 200
 	elif Input.is_action_pressed("ui_left"):
-		move.x = -100
+		move.x = -200
 	else:
 		move.x = 0
 		
 	if is_on_floor():
 		move.y = 0
 		if Input.is_action_pressed("ui_up"):
-			move.y = -300
+			move.y = -300 * 1.5
 	else:
-		move.y += 9.8
+		move.y += 9.8 * 2
 		
-	move_and_slide(move, Vector2.UP)
+	move_and_slide(move * delta * 80, Vector2.UP)
 
 func _on_Coin_coin_collected():
-	score += 10
-	print(score)
+	GameData.score += 10
+	ScoreLabel.set_text(str("Score: ", GameData.score))
